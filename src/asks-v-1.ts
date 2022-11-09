6b35ebaf7bf411e4a5469eeb6b654916e054b7f9
Colin Platt <colingplatt@gmail.com>
2022-11-09 20:50:27 +0100

    ts

--- src/asks-v-1.ts
+++ src/asks-v-1.ts
@@ -8,18 +8,52 @@ import {
   AskEvent
 } from "../generated/schema"
 
-export function handleAsk(event: AskEvent): void {
-  let entity = AskEvent.load(event.params.uuid.toHex())
+export function handleAskCanceled(event: AskCanceledEvent): void {
+  let entity = new AskEvent(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.tokenContract = event.params.tokenContract
+  entity.tokenId = event.params.tokenId
+  entity.ask_seller = event.params.ask.seller
+  entity.ask_sellerFundsRecipient = event.params.ask.sellerFundsRecipient
+  entity.ask_askCurrency = event.params.ask.askCurrency
+  entity.ask_findersFeeBps = event.params.ask.findersFeeBps
+  entity.ask_askPrice = event.params.ask.askPrice
+  entity.save()
+}
 
-  if (entity == null) {
-    entity = new Attestation(event.params.uuid.toHex())
-  }
+export function handleAskCreated(event: AskCreatedEvent): void {
+  let entity = new AskEvent(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.tokenContract = event.params.tokenContract
+  entity.tokenId = event.params.tokenId
+  entity.ask_seller = event.params.ask.seller
+  entity.ask_sellerFundsRecipient = event.params.ask.sellerFundsRecipient
+  entity.ask_askCurrency = event.params.ask.askCurrency
+  entity.ask_findersFeeBps = event.params.ask.findersFeeBps
+  entity.ask_askPrice = event.params.ask.askPrice
+  entity.save()
+}
 
+export function handleAskFilled(event: AskFilledEvent): void {
+  let entity = new AskEvent(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.tokenContract = event.params.tokenContract
+  entity.tokenId = event.params.tokenId
+  entity.ask_seller = event.params.ask.seller
+  entity.ask_sellerFundsRecipient = event.params.ask.sellerFundsRecipient
+  entity.ask_askCurrency = event.params.ask.askCurrency
+  entity.ask_findersFeeBps = event.params.ask.findersFeeBps
+  entity.ask_askPrice = event.params.ask.askPrice
+  entity.save()
+}
 
-
-
-
-
+export function handleAskPriceUpdated(event: AskPriceUpdatedEvent): void {
+  let entity = new AskEvent(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
   entity.tokenContract = event.params.tokenContract
   entity.tokenId = event.params.tokenId
   entity.ask_seller = event.params.ask.seller
